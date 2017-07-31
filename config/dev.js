3e58cd040fa464407cf3e5801578c2978d83c36a
JasonLinn <cheng11220@gmail.com>
2017-08-01 00:28:16 +0800

    deb prod

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+//dont commit this!
+module.exports = {
+    googleClientID:"18965006801-i4lto7frruqrbm7efkdtjejn1s6e8dqv.apps.googleusercontent.com",
+    googleClientSecret: "XHxD-zkk8_Ni2RxwdqZNbLdU",
+    mongoURI: 'mongodb://admin:admin@ds127963.mlab.com:27963/heroku_lpct1db2',
+    cookieKey:'sdlfjaksjflkjsdjflj'
+};
--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
-module.exports = {
-    googleClientID:"18965006801-i4lto7frruqrbm7efkdtjejn1s6e8dqv.apps.googleusercontent.com",
-    googleClientSecret: "XHxD-zkk8_Ni2RxwdqZNbLdU",
-    mongoURI: 'mongodb://admin:admin@ds127963.mlab.com:27963/heroku_lpct1db2',
-    cookieKey:'sdlfjaksjflkjsdjflj'
-};
+// firgure out what set of credentials to return
+if (process.env.NODE_ENV === 'production'){ //process.env.NODE_ENV heroku會產生給你
+    module.exports = require('./prod');
+}else{
+
+    module.exports = require('./dev');
+}
