4be04b8e967eca9d42d06a4b93c9e4b8887adaf9
JasonLinn <cheng11220@gmail.com>
2017-09-05 18:50:50 +0800

    logged in

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css'
+import React from 'react'
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk';
 
-import App from './components/App';
+/**
+ * Class
+ */
+import App from './components/App'
+import reducers from './reducers'
 
-ReactDOM.render(<App />,document.querySelector("#root"));
+/**
+ * Const
+ */
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
+
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>, // provider讓包在裡面的class擁有store
+    document.querySelector('#root')
+)
